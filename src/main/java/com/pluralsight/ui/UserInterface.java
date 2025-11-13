703f9d738e59e0272018cb8f2c1fb84aee808323
Ash <mendezashleyy@gmail.com>
2025-11-13 18:09:06 -0500

    added prompts for parts of Topping to UserInterface and added setters to Ramen

--- src/main/java/com/pluralsight/ui/UserInterface.java
+++ src/main/java/com/pluralsight/ui/UserInterface.java
@@ -1,7 +1,10 @@
 package com.pluralsight.ui;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import com.pluralsight.items.Order;
 import com.pluralsight.items.Ramen;
+import com.pluralsight.items.Topping;
 
 public class UserInterface {
     Scanner scanner = new Scanner(System.in);
@@ -67,11 +70,14 @@ public class UserInterface {
                 System.out.println("Guess you're not hungry today...");
                 return;
             }
-            Order order;
-            switch (option) {
+            Order order; // expand on Order to tie it in
+            switch (option) { // cases are work in progress lol
                 case 1 -> {
                     Ramen ramen = new Ramen(promptForSize(), promptForBroth(), promptForNoodle());
                     ramen.setSpicy(promptForSpicy());
+                    ramen.setMeats(promptForMeats());
+                    ramen.setVegetables(promptForVegetables());
+                    ramen.setPremiums(promptForPremiumToppings());
                 }
                 case 2 -> // add Drink
                 case 3 -> // add Appetizer
@@ -79,7 +85,7 @@ public class UserInterface {
             }
         }
     }
-
+// set up for Ramen base
     public int promptForSize() {
         for (Ramen.bowlSize b : Ramen.bowlSize.values()) {
             System.out.println(b.getName() + "  $" + b.getPrice());
@@ -107,5 +113,74 @@ public class UserInterface {
     public boolean promptForSpicy() {
         int ask = askUserInt("Would you like your broth spicy?\nPick 1 for yes, 0 for no: ");
         return ask == 1;
+    }
+
+    // returns list for toppings
+    public List<Topping.meat> promptForMeats(){
+        List<Topping.meat> meats = new ArrayList<>();
+        for (int i = 0; i < 2; i++) {
+            int[] valid = {0,1,2,3,4};
+            for(Topping.meat b: Topping.meat.values()){
+                System.out.println(b.getName());
+            }
+            int option = screenMenuValidation("\tThese are the meat options.\nPick a number or 0 if done: ",valid);
+            if (option == 0) {
+                System.out.println("Next option->");
+                break;
+            }
+            else {
+                meats.add(Topping.meat.values()[option-1]);
+                int[] valid2 = {1,0};
+                if ((screenMenuValidation("\tWould you like extra?\nPick 1 for yes, 0 for no: ", valid2)) == 1){
+                    meats.add(Topping.meat.values()[option-1]);
+                }
+            }
+        }
+        return meats;
+    }
+    public List<Topping.vegetable> promptForVegetables(){
+        List<Topping.vegetable> vegetables = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            int[] valid = {0,1,2,3,4};
+            for(Topping.vegetable b: Topping.vegetable.values()){
+                System.out.println(b.getName());
+            }
+            int option = screenMenuValidation("\tThese are the vegetable options.\nPick a number or 0 if done: ",valid);
+            if (option == 0) {
+                System.out.println("Next option->");
+                break;
+            }
+            else {
+                vegetables.add(Topping.vegetable.values()[option-1]);
+                int[] valid2 = {1,0};
+                if ((screenMenuValidation("\tWould you like extra?\nPick 1 for yes, 0 for no: ", valid2)) == 1){
+                    vegetables.add(Topping.vegetable.values()[option-1]);
+                }
+            }
+        }
+        return vegetables;
+    }
+
+    public List<Topping.premium> promptForPremiumToppings(){
+        List<Topping.premium> premium = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            int[] valid = {0,1,2,3,4};
+            for(Topping.premium b: Topping.premium.values()){
+                System.out.println(b.getName() + "  $" + b.getPrice());
+            }
+            int option = screenMenuValidation("\tThese are the premium topping options.\nPick a number or 0 if done: ",valid);
+            if (option == 0) {
+                System.out.println("Next option->");
+                break;
+            }
+            else {
+                premium.add(Topping.premium.values()[option-1]);
+                int[] valid2 = {1,0};
+                if ((screenMenuValidation("\tWould you like extra?\nPick 1 for yes, 0 for no: ", valid2)) == 1){
+                    premium.add(Topping.premium.values()[option-1]);
+                }
+            }
+        }
+        return premium;
     }
 }
