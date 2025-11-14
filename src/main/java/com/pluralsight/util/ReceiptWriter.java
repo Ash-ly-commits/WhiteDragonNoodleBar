c61ad4eff088f6e75b02d0e49144df5502450c37
Ash <mendezashleyy@gmail.com>
2025-11-14 08:39:25 -0500

    made sure new file receipts will be saved to src/main/resources/receipts/

--- src/main/java/com/pluralsight/util/ReceiptWriter.java
+++ src/main/java/com/pluralsight/util/ReceiptWriter.java
@@ -3,12 +3,15 @@ package com.pluralsight.util;
 import com.pluralsight.items.Order;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class ReceiptWriter {
+    private static final String RECEIPT_DIR = "src/main/resources/receipts/";
+
     private String generateTimestamp() {
         LocalDateTime rightNow = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
@@ -16,11 +19,19 @@ public class ReceiptWriter {
     }
 
     public void saveReceipt(Order order) {
-        String txtFileName = generateTimestamp() + ".txt";
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(txtFileName))) {
+        File directory = new File(RECEIPT_DIR);
+        if (!directory.exists()) {
+            boolean created = directory.mkdirs();
+            if (!created) {
+                System.err.println("Could not create receipts directory: " + directory.getAbsolutePath());
+            }
+        }
+        String filePath = RECEIPT_DIR + generateTimestamp() + ".txt";
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
             writer.write(order.getOrderSummary());
         } catch (IOException e) {
-            System.err.println("Error writing to file: " + e.getMessage());
+            System.err.println("Error writing receipt: " + e.getMessage());
         }
     }
 }
