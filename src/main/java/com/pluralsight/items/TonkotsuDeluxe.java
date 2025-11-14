e52847f943bf142ec96151a9adc5c45f60b3a662
Ash <mendezashleyy@gmail.com>
2025-11-14 01:03:58 -0500

    added constructor to set up default values for TonkotsuDeluxe signature class

--- src/main/java/com/pluralsight/items/TonkotsuDeluxe.java
+++ src/main/java/com/pluralsight/items/TonkotsuDeluxe.java
@@ -1,4 +1,26 @@
 package com.pluralsight.items;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TonkotsuDeluxe extends Ramen{
+    public TonkotsuDeluxe() {
+        super(2, 1, 1);
+
+        // sets the default toppings
+        List<Topping.meat> meats = new ArrayList<>();
+        meats.add(Topping.meat.CHASHU);
+
+        List<Topping.vegetable> veggies = new ArrayList<>();
+        veggies.add(Topping.vegetable.SHIITAKE);
+
+        List<Topping.premium> premiums = new ArrayList<>();
+        premiums.add(Topping.premium.EGG);
+
+        setMeats(meats);
+        setVegetables(veggies);
+        setPremiums(premiums);
+
+        setSpicy(false);
+    }
 }
