f5328f2a41ea77edafbd1c0c1d135da1ec371804
Ash <mendezashleyy@gmail.com>
2025-11-13 19:16:55 -0500

    added enum types to Drink

--- src/main/java/com/pluralsight/items/Drink.java
+++ src/main/java/com/pluralsight/items/Drink.java
@@ -1,5 +1,53 @@
 package com.pluralsight.items;
 
 public class Drink{
+    public enum drinkSize{
+        SMALL("Small - 16 fl oz", 0),
+        MEDIUM("Medium - 20 fl oz", 0),
+        LARGE("Large - 24 fl oz", 2);
 
+        private final String name;
+        private final int price;
+
+        drinkSize(String name, int price){
+            this.name = name;
+            this.price = price;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public int getPrice(){
+            return price;
+        }
+    }
+
+    public enum drinkType{
+        WATER("Water",0),
+        COKE("Coke (NOT Diet)",3),
+        PEPSI("Pepsi (NOT Diet)",3),
+        MATCHA("Matcha - warm",6),
+        RAMUNE("Ramune - Japanese citrus soft drink",4),
+        SAPPORO("Sapporo - Japanese lager",7),
+        ASAHI("Asahi Super Dry - Japanese pale lager",7),
+        SAKE("Sake - Japanese alcohol",7),
+        SOJU("Soju - Korean distilled spirit",7);
+
+        private final String name;
+        private final int price;
+
+        drinkType(String name, int price){
+            this.name = name;
+            this.price = price;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public int getPrice(){
+            return price;
+        }
+    }
 }
