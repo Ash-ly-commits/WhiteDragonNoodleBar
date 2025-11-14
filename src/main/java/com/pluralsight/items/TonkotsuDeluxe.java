695c9a1c0dfff7bdf4e30d802d8fa0d1733503d8
Ash <mendezashleyy@gmail.com>
2025-11-14 01:19:23 -0500

    added methods to customize toppings for TonkotsuDeluxe signature class

--- src/main/java/com/pluralsight/items/TonkotsuDeluxe.java
+++ src/main/java/com/pluralsight/items/TonkotsuDeluxe.java
@@ -13,6 +13,7 @@ public class TonkotsuDeluxe extends Ramen{
 
         List<Topping.vegetable> veggies = new ArrayList<>();
         veggies.add(Topping.vegetable.SHIITAKE);
+        veggies.add(Topping.vegetable.NORI);
 
         List<Topping.premium> premiums = new ArrayList<>();
         premiums.add(Topping.premium.EGG);
@@ -22,5 +23,45 @@ public class TonkotsuDeluxe extends Ramen{
         setPremiums(premiums);
 
         setSpicy(false);
+    }
+
+    // customization helpers
+    public void addMeat(Topping.meat m) {
+        List<Topping.meat> list = getMeats();
+        if (list == null) {
+            list = new ArrayList<>();
+            setMeats(list); }
+        list.add(m);
+    }
+
+    public void removeMeat(Topping.meat m) {
+        List<Topping.meat> list = getMeats();
+        if (list != null) list.remove(m);
+    }
+
+    public void addVegetable(Topping.vegetable v) {
+        List<Topping.vegetable> list = getVegetables();
+        if (list == null) {
+            list = new ArrayList<>();
+            setVegetables(list); }
+        list.add(v);
+    }
+
+    public void removeVegetable(Topping.vegetable v) {
+        List<Topping.vegetable> list = getVegetables();
+        if (list != null) list.remove(v);
+    }
+
+    public void addPremium(Topping.premium p) {
+        List<Topping.premium> list = getPremiums();
+        if (list == null) {
+            list = new ArrayList<>();
+            setPremiums(list); }
+        list.add(p);
+    }
+
+    public void removePremium(Topping.premium p) {
+        List<Topping.premium> list = getPremiums();
+        if (list != null) list.remove(p);
     }
 }
